298f0eef178910967d146c57559ec194dcb903d9
prsloan <prsloan@csupomona.edu>
2015-09-11 13:34:07 -0700

    test

--- js/app.js
+++ js/app.js
@@ -6,9 +6,23 @@
     this.product = gem;
   });
 
-  var gem = {
-    name : 'Dodecahedron',
-    price : 2.95,
-    description : 'Twelve Sides',
-  }
+  var gems = [
+    {
+      name : 'Dodecahedron',
+      price : 2.95,
+      description : 'Twelve Sides',
+      canPurchase : true ,
+    },
+
+    {
+      name : "Pentagonal Gem",
+      price : 5.95,
+      description : "It's gotta have 5 sides baby."
+      canPurchase : false ,
+    }
+
+
+
+
+
 })();
