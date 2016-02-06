d63e6bb096c1ef007589b9838947542e32db50f4
prsloan <prsloan@csupomona.edu>
2016-02-06 14:20:05 -0800

    test

---
+++ ClarifaiTest/clari.js
@@ -0,0 +1,5 @@
+
+
+var access_token = 'jFatxmTOad5o0dBueEypwxpd0jvUeG';
+var header = "Authorization: Bearer " + access_token;
+      https://api.clarifai.com/v1/tag/?url=
--- js/app.js
+++ js/app.js
@@ -1,14 +1,12 @@
-angular.module('website', ['ngAnimate'])
-    .controller('MainCtrl', function ($scope) { });
-
 
     angular.module('website', ['ngAnimate'])
         .controller('MainCtrl', function ($scope) {
             $scope.slides = [
                 {image: 'img/test/test-1.jpg', description: 'Image 00'},
-                {image: 'img/test/test-1.jpg', description: 'Image 01'},
-                {image: 'img/test/test-1.jpg', description: 'Image 02'},
-                {image: 'img/test/test-1.jpg', description: 'Image 03'},
-                {image: 'img/test/test-1.jpg', description: 'Image 04'}
+                {image: 'img/test/test-2.jpg', description: 'Image 01'},
+                {image: 'img/test/test-3.jpg', description: 'Image 02'},
+                {image: 'img/test/test-4.jpg', description: 'Image 03'},
+                {image: 'img/test/test-5.jpg', description: 'Image 04'},
+                {image: 'img/test/test-6.jpg', description: 'Image 04'},
             ];
         });
