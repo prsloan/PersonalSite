c34cf3454d7433dacf9170da2753947e68aaefb6
prsloan <prsloan@csupomona.edu>
2016-02-06 14:25:06 -0800

    test

--- js/app.js
+++ js/app.js
@@ -9,4 +9,60 @@
                 {image: 'img/test/test-5.jpg', description: 'Image 04'},
                 {image: 'img/test/test-6.jpg', description: 'Image 04'},
             ];
-        });
+            $scope.direction = 'left';
+           $scope.currentIndex = 0;
+
+           $scope.setCurrentSlideIndex = function (index) {
+               $scope.direction = (index > $scope.currentIndex) ? 'left' : 'right';
+               $scope.currentIndex = index;
+           };
+
+           $scope.isCurrentSlideIndex = function (index) {
+               return $scope.currentIndex === index;
+           };
+
+           $scope.prevSlide = function () {
+               $scope.direction = 'left';
+               $scope.currentIndex = ($scope.currentIndex < $scope.slides.length - 1) ? ++$scope.currentIndex : 0;
+           };
+
+           $scope.nextSlide = function () {
+               $scope.direction = 'right';
+               $scope.currentIndex = ($scope.currentIndex > 0) ? --$scope.currentIndex : $scope.slides.length - 1;
+           };
+       })
+       .animation('.slide-animation', function () {
+        return {
+            beforeAddClass: function (element, className, done) {
+                var scope = element.scope();
+
+                if (className == 'ng-hide') {
+                    var finishPoint = element.parent().width();
+                    if(scope.direction !== 'right') {
+                        finishPoint = -finishPoint;
+                    }
+                    TweenMax.to(element, 0.5, {left: finishPoint, onComplete: done });
+                }
+                else {
+                    done();
+                }
+            },
+            removeClass: function (element, className, done) {
+                var scope = element.scope();
+
+                if (className == 'ng-hide') {
+                    element.removeClass('ng-hide');
+
+                    var startPoint = element.parent().width();
+                    if(scope.direction === 'right') {
+                        startPoint = -startPoint;
+                    }
+
+                    TweenMax.fromTo(element, 0.5, { left: startPoint }, {left: 0, onComplete: done });
+                }
+                else {
+                    done();
+                }
+            }
+        };
+    });
