69320b6e0197178d985249f754b76bd50e292dd5
prsloan <prsloan@csupomona.edu>
2016-02-07 10:07:17 -0800

    trying something new

--- js/app.js
+++ js/app.js
@@ -24,7 +24,7 @@ var finalTags;
       ) {
 
 var AllTheData = {
-  'dataPoints' : new Array(250),
+  'dataPoints' : new Array(500),
   'geometry' : [],
   'title' : [],
   'description': [],
@@ -112,7 +112,6 @@ function run(imgurl) {
       'data': data,
       'type': 'POST'
     }).then(function(r) {
-      console.log(data.url);
       parseResponse(r, data.url);
     }).then(function(r){
       if(AllTheData.populated.length == 250){
@@ -236,7 +235,7 @@ function run(imgurl) {
         //get geotagged photos from flickr
         //tags=flower&tagmode=all
         var requestHandle = esriRequest({
-          url: "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=1c3ec063d7ec928228bb799d059ba282&min_upload_date=1449459707&max_upload_date=1423280507&has_geo=1&per_page=250&page=1&format=json&nojsoncallback=0",
+          url: "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=1c3ec063d7ec928228bb799d059ba282&min_upload_date=1449459707&max_upload_date=1423280507&has_geo=1&per_page=500&page=1&format=json&nojsoncallback=0",
           callbackParamName: "jsoncallback"
         });
         requestHandle.then(requestSucceeded, requestFailed);
