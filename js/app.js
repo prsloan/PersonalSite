5beb84adf16f1c9cd07625a4514d8e2a767f8adc
prsloan <prsloan@csupomona.edu>
2016-02-07 03:30:45 -0800

    trying something new

--- js/app.js
+++ js/app.js
@@ -225,7 +225,7 @@ function run(imgurl) {
             requestHandle2.then( function(response, io){
               run(url);
               var geometry = new Point(response.photo.location.longitude, response.photo.location.latitude);
-              }).then( function(gerometry){
+            }).then( function(geometry){
               console.log(JSON.stringify(geometry));
               attr["description"] = "<p><a href=\"http://www.flickr.com/photos/"+item.owner+"/"+id+"/\"><img src=\""+url+"\" \"width = \"240\" height=\"160\" /><\/a><\/p><p><b>Keywords :<\/b>"+finalTags+" <\/p>" ;
               attr["title"] = item.title ? item.title : "Flickr Photo";
